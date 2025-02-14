468d2ee46dc93fdc1300b0d09eb8073c0776ea85
rptl8sr <rptl8sr@gmail.com>
2025-02-15 02:06:58 +0300

    Add main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"merch-shop/internal/app"
+	"merch-shop/pkg/logger"
+)
+
+func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	a := app.Must(ctx)
+
+	go func() {
+		if err := a.Run(ctx); err != nil {
+			logger.Error("app.Run: ", "message", err.Error())
+			stop()
+		}
+	}()
+
+	<-ctx.Done()
+	logger.Info("Shutdown signal received")
+
+	if err := a.Shutdown(); err != nil {
+		logger.Error("app.Close: ", "message", err.Error())
+	}
+
+	logger.Info("Shutdown complete")
+}
